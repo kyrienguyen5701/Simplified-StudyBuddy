de593506f805b856aca1046b2487344a5dee943d
Kyrie Nguyen <sonnhthanglong@gmail.com>
2023-04-14 00:34:09 -0400

    Add activity feed and editable user profile

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
@@ -6,33 +7,44 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 # Create your views here.
 def home(request):
   q = ''
   if request.GET.get('q'):
     q = request.GET.get('q')
+  
   rooms = Room.objects.filter(
     Q(topic__name__icontains=q) |
     Q(name__icontains=q) |
     Q(description__icontains=q)
   )
+  
   topics = Topic.objects.all()[:5]
   room_count = rooms.count() # faster than len
-  ctx = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+
+  # get recent activities only for the current room in view
+  recent_activities = Message.objects.filter(Q(room__topic__name__icontains=q))
+  
+  ctx = {
+    'rooms': rooms,
+    'topics': topics,
+    'room_count': room_count,
+    'recent_activities': recent_activities
+  }
   return render(request, 'base/home.html', ctx)
 
 def room(request, id):
   room = Room.objects.get(id=id)
-  msgs = room.message_set.all().order_by('-created')
+  msgs = room.message_set.all()
   participants = room.participants.all()
 
   if request.method == 'POST':
     msg = Message.objects.create(
       user=request.user,
       room=room,
-      body=request.POST.get('body')
+      content=request.POST.get('content')
     )
 
     # add a new user once they comment
@@ -40,7 +52,11 @@ def room(request, id):
 
     return redirect('room', id=room.id)
   
-  ctx = {'room': room, 'msgs': msgs, 'participants': participants}
+  ctx = {
+    'room': room,
+    'msgs': msgs,
+    'participants': participants
+  }
   return render(request, 'base/room.html', ctx)
 
 @login_required(login_url='login')
@@ -53,10 +69,14 @@ def create_room(request):
 
     # check if the form is valid, save, and redirect
     if form.is_valid():
-      form.save()
+      room = form.save(commit=False)
+      room.host = request.user
+      room.save()
       return redirect('home')
 
-  ctx = {'form': form}
+  ctx = {
+    'form': form
+  }
   return render(request, 'base/room_form.html', ctx)
 
 @login_required(login_url='login')
@@ -66,26 +86,42 @@ def update_room(request, id):
   # prefill the form
   form = RoomForm(instance=room)
 
+  topics = Topic.objects.all()
+
+  if request.user != room.host:
+    return HttpResponse('You are not allowed here')
+
   if request.method == 'POST':
-    # what room to update
-    form = RoomForm(request.POST, instance=room)
+    topic_name = request.POST.get('topic')
+    topic, _ = Topic.objects.get_or_create(name=topic_name)
+    room.name = request.POST.get('name')
+    room.topic = topic
+    room.description = request.POST.get('description')
+    room.save()
+    return redirect('home')
 
-    if form.is_valid():
-      form.save()
-      return redirect('home')
-
-  ctx = {'room': room}
+  ctx = {
+    'room': room,
+    'form': form,
+    'topics': topics
+  }
   return render(request, 'base/room_form.html', ctx)
 
 @login_required(login_url='login')
 def delete_room(request, id):
   room = Room.objects.get(id=id)
 
+  if request.user != room.host:
+    return HttpResponse('You are not allowed here')
+
   if request.method == 'POST':
     room.delete()
     return redirect('home')
   
-  return render(request, 'base/delete.html', {'obj': room})
+  ctx = {
+    'obj': room
+  }
+  return render(request, 'base/delete.html', ctx)
 
 def login_page(request):
   page = 'login'
@@ -143,10 +179,18 @@ def register_page(request):
 @login_required(login_url='login')
 def delete_msg(request, id):
   msg = Message.objects.get(id=id)
+
+  if request.user != msg.user:
+    return HttpResponse('You are not allowed here')
+
   if request.method == 'POST':
     msg.delete()
     return redirect('home')
-  return render(request, 'base/delete.html', {'obj': msg})
+  
+  ctx = {
+    'obj': msg
+  }
+  return render(request, 'base/delete.html', ctx)
 
 @login_required(login_url='login')
 def edit_msg(request, id):
@@ -160,4 +204,37 @@ def edit_msg(request, id):
     
     msg.save()
     return redirect('room', id=msg.room.id)
-  return render(request, 'base/edit.html', {'obj': msg})
+  
+  ctx = {
+    'obj': msg
+  }
+  return render(request, 'base/edit.html', ctx)
+
+
+def user_profile(request, id):
+  user = User.objects.get(id=id)
+  rooms = user.room_set.all()
+  msgs = user.message_set.all()
+  topics = Topic.objects.all()
+
+  ctx = {
+    'user': user,
+    'rooms': rooms,
+    'msgs': msgs,
+    'topics': topics
+  }
+  return render(request, 'base/profile.html', ctx)
+
+@login_required(login_url='login')
+def update_user(request):
+  user = request.user
+  form = UserForm(instance=user)
+
+  if request.method == 'POST':
+    form = UserForm(request.POST, instance=user)
+    if form.is_valid():
+      form.save()
+      return redirect('user-profile', id=user.id)
+
+  ctx = {'form': form}
+  return render(request, 'base/update_user.html', ctx)
